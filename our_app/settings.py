fe6960830c00b523749367136bfa5269a10a6031
Kopesh <kopesh@example.com>
2026-04-19 03:38:56 +0530

    data added

--- our_app/settings.py
+++ our_app/settings.py
@@ -171,4 +171,13 @@ LOGGING = {
           },
       }
 
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT="587"
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER=os.getenv("email_id")
+EMAIL_HOST_PASSWORD=os.getenv("email")
+
+
+
 
